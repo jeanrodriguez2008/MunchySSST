35c6d40b20c096fbf7155e221eeee2367b52ea46
Jean Carlos Rodriguez <jeanrodriguez2008@gmail.com>
2026-08-27 10:39:34 -0400

    Corregir rutas de iconos icon-192 y icon-512 en manifest.json

--- app/static/service-worker.js
+++ app/static/service-worker.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = 'munchyssst-cache-v1';
+const CACHE_NAME = 'munchyssst-cache-v2';
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         '/login',
+        '/static/img/icon-192.png',
+        '/static/img/icon-512.png',
         '/static/img/logo_header.png'
       ]);
     })
